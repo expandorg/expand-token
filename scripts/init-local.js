01504c073d8fe56c355421c2cd06b02510cb1b94
Jascha Ephraim <jascha@jaschaephraim.com>
2018-03-19 01:14:25 -0700

    Update scripts

---
+++ scripts/init-local.js
@@ -0,0 +1,10 @@
+const runScript = require('../src/runScript');
+
+runScript(async (token, ownerAddress, web3) => {
+  const vaultAddress = '0x5f3ebfe1fc670018e121224bb95007d8b498359b';
+  const deposit = 10000*1e+18;
+  const log = await token.transfer(vaultAddress, deposit);
+  console.log(log);
+})
+  .then(() => console.log('done'))
+  .catch(err => console.error(err));
--- scripts/template.js
+++ scripts/template.js
@@ -1,7 +1,7 @@
 const runScript = require('../src/runScript');
 
 runScript(async (token, ownerAddress, web3) => {
-
+  // Script here
 })
   .then(() => console.log('done'))
   .catch(err => console.error(err));
