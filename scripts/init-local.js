a17da183cca2e5cfd22e617ec27b0bcd6d3436b3
Jascha Ephraim <jascha@jaschaephraim.com>
2018-07-09 15:22:19 -0700

    Add vault approval to local init

--- scripts/init-local.js
+++ scripts/init-local.js
@@ -2,9 +2,13 @@ const runScript = require('../src/runScript');
 
 runScript(async (token, ownerAddress, web3, watcher) => {
   const vaultAddress = '0x5f3ebfe1fc670018e121224bb95007d8b498359b';
-  const deposit = 10000e+18;
-  const log = await token.transfer(vaultAddress, deposit);
-  console.log(log);
+  const deposit = 10000e18;
+  const vaultTransferLog = await token.transfer(vaultAddress, deposit);
+  console.log(vaultTransferLog);
+
+  const approve = 1000e18;
+  const tokenAllowLog = await token.approve(vaultAddress, approve);
+  console.log(tokenAllowLog);
 })
   .then(() => console.log('done'))
   .catch(err => console.error(err));
