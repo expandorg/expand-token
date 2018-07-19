667e3168772c42e27098ecaaca9055db0662a04b
Jascha Ephraim <jascha@jaschaephraim.com>
2018-07-19 14:45:10 -0700

    Update local init script

--- scripts/init-local.js
+++ scripts/init-local.js
@@ -3,15 +3,19 @@ const runScript = require('../src/runScript');
 
 runScript(async (token, ownerAddress, web3, watcher) => {
   const vaultAddress = '0x5f3ebfe1fc670018e121224bb95007d8b498359b';
-  const deposit = 10000e18;
-  const vaultTransferLog = await token.transfer(vaultAddress, deposit);
-  console.log(vaultTransferLog);
-
   const worker1Address = '0xcdfc424daeea84e51b8f5887196d0209fe9289b2';
+  const deposit = 10000e18;
+  const approve = 1000e18;
+
   const worker1Token = new GemsToken(web3.currentProvider, worker1Address);
   await worker1Token.init();
 
-  const approve = 1000e18;
+  const vaultTransferLog = await token.transfer(vaultAddress, deposit);
+  console.log(vaultTransferLog);
+
+  const worker1TransferLog = await token.transfer(worker1Address, deposit);
+  console.log(worker1TransferLog);
+
   const worker1AllowLog = await worker1Token.approve(vaultAddress, approve);
   console.log(worker1AllowLog);
 })
