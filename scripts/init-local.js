cafa2d124620354887692b93206595d00e99082a
Jascha Ephraim <jascha@jaschaephraim.com>
2018-07-19 13:06:21 -0700

    Update local init script

--- scripts/init-local.js
+++ scripts/init-local.js
@@ -6,13 +6,13 @@ runScript(async (token, ownerAddress, web3, watcher) => {
   const vaultTransferLog = await token.transfer(vaultAddress, deposit);
   console.log(vaultTransferLog);
 
-  const approve = 1000e18;
-  const tokenAllowLog = await token.approve(vaultAddress, approve);
-  console.log(tokenAllowLog);
-
   const worker2Address = '0x60e156ec48336b69fc0628ae357f50735fd7be6e';
   const worker2TransferLog = await token.transfer(worker2Address, deposit);
   console.log(worker2TransferLog);
+
+  const approve = 2 * 1000e18;
+  const tokenAllowLog = await token.approve(vaultAddress, approve);
+  console.log(tokenAllowLog);
 })
   .then(() => console.log('done'))
   .catch(err => console.error(err));
