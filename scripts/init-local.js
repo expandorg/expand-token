e4a90be3de1788fb71a97a980c3598d5efca3148
Jascha Ephraim <jascha@jaschaephraim.com>
2018-07-23 08:38:47 -0400

    Update local init script

--- scripts/init-local.js
+++ scripts/init-local.js
@@ -1,18 +1,22 @@
 const { GemsToken } = require('../src/GemsToken');
 const runScript = require('../src/runScript');
+const accounts = require('../../accounts/local/addresses.json');
 
 runScript(async (token, ownerAddress, web3, watcher) => {
-  const vaultAddress = '0x5f3ebfe1fc670018e121224bb95007d8b498359b';
-  const worker1Address = '0xcdfc424daeea84e51b8f5887196d0209fe9289b2';
-  const worker2Address = '0x9e15a781d26dda1aef3eeb0016bbc199d0bc8b1c';
   const deposit = 10000e18;
   const approve = 1000e18;
 
-  const worker1Token = new GemsToken(web3.currentProvider, worker1Address);
+  const worker1Token = new GemsToken(web3.currentProvider, accounts.worker1.address);
   await worker1Token.init();
 
-  const worker2Token = new GemsToken(web3.currentProvider, worker2Address);
+  const worker2Token = new GemsToken(web3.currentProvider, accounts.worker2.address);
   await worker2Token.init();
+
+  const requester1Token = new GemsToken(web3.currentProvider, accounts.requester1.address);
+  await requester1Token.init();
+
+  const requester2Token = new GemsToken(web3.currentProvider, accounts.requester2.address);
+  await requester2Token.init();
 
   const vaultTransferLog = await token.transfer(vaultAddress, deposit);
   console.log(vaultTransferLog);
@@ -23,11 +27,23 @@ runScript(async (token, ownerAddress, web3, watcher) => {
   const worker2TransferLog = await token.transfer(worker2Address, deposit);
   console.log(worker2TransferLog);
 
+  const requester1TransferLog = await token.transfer(requester1Address, deposit);
+  console.log(requester1TransferLog);
+
+  const requester2TransferLog = await token.transfer(requester2Address, deposit);
+  console.log(requester2TransferLog);
+
   const worker1AllowLog = await worker1Token.approve(vaultAddress, approve);
   console.log(worker1AllowLog);
 
   const worker2AllowLog = await worker2Token.approve(vaultAddress, approve);
   console.log(worker2AllowLog);
+
+  const requester1AllowLog = await requester1Token.approve(vaultAddress, approve);
+  console.log(requester1AllowLog);
+
+  const requester2AllowLog = await requester2Token.approve(vaultAddress, approve);
+  console.log(requester2AllowLog);
 })
   .then(() => console.log('done'))
   .catch(err => console.error(err));
