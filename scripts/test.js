5a1ad4a7a8c6c371682b90cd277749c56d2f96a6
Jascha Ephraim <jascha@jaschaephraim.com>
2018-09-03 14:56:05 -0700

    Update tests for compose environment'

--- scripts/test.js
+++ scripts/test.js
@@ -3,7 +3,8 @@ const Big = require('bignumber.js');
 const runScript = require('../src/runScript');
 
 const totalSupply = new Big('8e27');
-const balance = new Big('10000e18');
+const distributedBalance = new Big('5e22');
+const initialUserBalance = new Big('1e22');
 const value = 1e9;
 
 async function assertRevert(fn) {
@@ -27,27 +28,27 @@ async function assertNoError(fn) {
 }
 
 runScript(async (token, ownerAddress, web3, watcher) => {
-  const aAddress = web3.eth.accounts[1];
-  const bAddress = web3.eth.accounts[2];
+  const aAddress = web3.eth.accounts[4];
+  const bAddress = web3.eth.accounts[5];
 
   // inital owner balance should equal total supply
   const ownerBalance = await token.balanceOf(ownerAddress);
-  assert(ownerBalance.eq(totalSupply.minus(balance)));
+  assert(ownerBalance.eq(totalSupply.minus(distributedBalance)));
 
   // initial user a balance should equal zero
   const aBalance = await token.balanceOf(aAddress);
-  assert(aBalance.eq(0));
+  assert(aBalance.eq(initialUserBalance));
 
   // initial user b balance should equal zero
   const bBalance = await token.balanceOf(bAddress);
-  assert(bBalance.eq(0));
+  assert(bBalance.eq(initialUserBalance));
 
   // transfer from owner to user a should not throw error
   await assertNoError(() => token.transfer(aAddress, value));
 
   // user a balance should be updated correctly
   const aNewBalance = await token.balanceOf(aAddress);
-  assert(aNewBalance.eq(value));
+  assert(aNewBalance.eq(initialUserBalance.plus(value)));
 
   // transfer from a to b should fail without approval
   await assertRevert(() => token.transferFrom(aAddress, bAddress, value));
