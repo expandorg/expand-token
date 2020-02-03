f02e395e51c065e24a8ebfc63d4c9476004832f1
Kieran O'Reilly <kieran@gems.org>
2020-02-03 14:47:35 -0500

    Reclaim Tokens

---
+++ contracts/CanReclaimToken.sol
@@ -0,0 +1,15 @@
+import "@openzeppelin/contracts/ownership/Ownable.sol";
+
+contract CanReclaimToken is Ownable {
+  using SafeERC20 for ERC20Basic;
+
+  /**
+   * @dev Reclaim all ERC20Basic compatible tokens
+   * @param token ERC20Basic The address of the token contract
+   */
+  function reclaimToken(ERC20Basic token) external onlyOwner {
+    uint256 balance = token.balanceOf(this);
+    token.safeTransfer(owner, balance);
+  }
+
+}
--- contracts/ExpandToken.sol
+++ contracts/ExpandToken.sol
@@ -2,8 +2,9 @@ pragma solidity ^0.5.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
+import "@gemsorg/gems-token/contracts/CanReclaimToken.sol";
 
-contract ExpandToken is ERC20, ERC20Detailed {
+contract ExpandToken is ERC20, ERC20Detailed, CanReclaimToken {
     constructor() ERC20Detailed("Expand Token", "XPN", 18) public {
         _mint(msg.sender, ((8*10**9)*10**18));
     }
